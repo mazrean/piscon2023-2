b2e2bea00eb1c4b44ac0dce36265ef32342a092f
mazrean <shunsuke.w04@gmail.com>
2023-11-05 22:07:14 +0900

    fix trend worker

--- app/go/main.go
+++ app/go/main.go
@@ -1209,7 +1209,7 @@ var trendCache, _ = isucache.New[int, []byte]("trend", func(ctx context.Context,
 	}
 
 	return json.Marshal(res)
-}, 0, time.Millisecond*10)
+}, 0, time.Millisecond*25)
 
 // GET /api/trend
 // ISUの性格毎の最新のコンディション情報
